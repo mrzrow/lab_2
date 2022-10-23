2b534ac0913acb155431e4bd9ad16fcec1a1cc71
zrow <mrzrow@yandex.ru>
2022-10-24 02:25:13 +0300

    rewrite forward() with operators precedence; add --reverse and --forward parameters

--- functions.cpp
+++ functions.cpp
@@ -1,31 +1,83 @@
 #include <iostream>
 
-int check_op(char op, int a, int b) {     
-    if (op == '+')
-        a = a + b;
-    else if (op == '-')
-        a = a - b; 
-    else if (op == '*')
-        a = a * b;
-    else
-        a = a / b;
+int is_digit(char a) {
+    if (a >= '0' and a <= '9')
+        return 1;
+    return 0;
+}
 
-    return a;
+int is_op(char a) {
+    if (a == '+' || a == '-' || 
+        a == '*' || a == '/')
+        return 1;
+    return 0;
+}
+
+int use_op(int a, int b, char op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return a / b;    
+    }
+    return -1;
+}
+
+int preceden(char a, char b) {
+    if ((a == '*' || a == '/') &&
+        (b == '*' || b == '/'))
+        return 1;
+    else if ((b == '*' || b == '/') &&
+             (a == '+' || a == '-'))
+        return 1;
+    else if ((a == '+' || a == '-') &&
+             (b == '+' || b == '-'))
+        return 1;
+    return 0;
 }
 
 int forward() {
-    int a, b;
+    char symbols[100];
+    int numbers[100];
+    int top_s = 0; int top_n = 0;
+    char token;
     char op;
-    std::cin >> a;
-    
-    while (std::cin >> op) {
-        std::cin >> b;
-        a = check_op(op, a, b);
+    int a, b;
+    while (std::cin >> token) {
+        if (is_digit(token))
+            numbers[top_n++] = token - '0';
+        else if (is_op(token)) {
+            while (top_s != 0 && 
+                   preceden(token, symbols[top_s - 1])) {
+                op = symbols[--top_s];
+                a = numbers[--top_n];
+                b = numbers[--top_n];
+                numbers[top_n++] = use_op(b, a, op);
+            }
+            symbols[top_s++] = token;
+        }
+        else if (token == '(')
+            symbols[top_s++] = token;
+        else if (token == ')'){
+            while (symbols[top_s - 1] != '(') {
+                a = numbers[--top_n];
+                b = numbers[--top_n];
+                op = symbols[--top_s];
+                numbers[top_n++] = use_op(b, a, op);
+            }
+            top_s--;
+        }
     }
-    
-    std::cout << std::endl << a << std::endl;
+    while (top_s != 0) {
+        op = symbols[--top_s];
+        a = numbers[--top_n];
+        b = numbers[--top_n];
+        numbers[top_n++] = use_op(b, a, op);
+    }  
+    std::cout << std::endl << numbers[--top_n] << std::endl;
     return 0;
 }
+
 
 int reverse() {
     int a, b;
@@ -34,8 +86,7 @@ int reverse() {
     
     while (std::cin >> b) {
         std::cin >> op;
-        a = check_op(op, a, b);
-    }
+        a = use_op(a, b, op);    }
 
     std::cout << std::endl << a << std::endl;
     return 0;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string.h>
 #include "functions.h"
 
-int main() {
+int main(int argc, char ** argv) {
+    if (argc == 1) {
+        std::cerr << "Nothing was read\n";
+        return 1;
+    }
+
+    for (std::size_t i = 1; i <= argc; ++i) {
+        if (strcmp(argv[i], "--forward") == 0)
+            forward();
+        if (strcmp(argv[i], "--reverse") == 0)
+            reverse();
+    }
     return 0;
 }
